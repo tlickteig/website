987c23d2706620923c1e78b1ae214082c907fbb6
tlickteig <57294226+tlickteig@users.noreply.github.com>
2020-02-25 12:08:36 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <script src="js/canvasjs.min.js" type="text/javascript"></script>
         <script src="js/standard.js" type="text/javascript"></script>
         <script src="js/meter.js" type="text/javascript"></script>
+        <script src="js/calculator.js" type="text/javascript"></script>
     </head>    
     <body  onload="start()"> 
         <header>
@@ -23,11 +24,12 @@
                 <ul>
                     <li><a href="#dvAbout">About</a></li>
                     <li><a href="#dvUsefulCharts">Useful Charts</a></li>
-                    <li><a href="#dvJsThing">Cool JavaScript thing</a></li>
+                    <li><a href="#dvJsThing">Cool JavaScript Thing</a></li>
                     <li><a href="#dvUnixClock">Unix Time Now</a></li>
                     <li><a href="#dvWorldClock">World Clock</a></li>
                     <li><a href="#dvWeather">Weather</a></li>
                     <li><a href="#dvBulletinBoard">Bulletin Board</a></li>
+                    <li><a href="#dvCalculator">Calculator</a></li>
                 </ul>
                 <div id="dvAbout">
                     <h2>Welcome!</h2>
@@ -43,9 +45,8 @@
                     <br />
                     <h3>View Counter</h3>
                     <?php
-                    
                     //Declare variables
-                    $db = mysqli_connect('localhost', '*', '*', 'bulletinBoard');
+                    $db = mysqli_connect('localhost', 'web', 'webaccess', 'bulletinBoard');
                     $query = "CALL bulletinBoard.sp_get_views();";
 
                     //Test for any connection errors
@@ -55,11 +56,11 @@
 
                         //Query the results
                         $result = $db->query($query);
-                        
+
                         //Test to see if there are any results
                         if ($result->num_rows > 0) {
-                            
-                           //Access the results and append to the page
+
+                            //Access the results and append to the page
                             $outputString = "";
                             while ($row = $result->fetch_assoc()) {
                                 $views = $row['views'];
@@ -67,7 +68,7 @@
                                 $outputString .= $views;
                                 $outputString .= "</p>";
                             }
-                            echo $outputString; 
+                            echo $outputString;
                         }
                     }
                     ?>
@@ -90,6 +91,14 @@
                         <div id="dvAccordionPersonalCalculators">
                             <div id="dvPersonalCalculators"></div>
                         </div>
+                        <h3>Number of Programming Languages I Know</h3>
+                        <div id="dvAccordionNumberOfProgrammingLanguages">
+                            <div id="dvNumberOfProgrammingLanguages"></div>
+                        </div>
+                        <h3>Sixty Watt Light Bulbs</h3>
+                        <div id="dvAccordionSixtyWattLightBulbs">
+                            <div id="dvSixtyWattLightBulbs"></div>
+                        </div>
                         <h3>How the Weather Feels in Iowa</h3>
                         <div id="dvAccordionIowaWeather">
                             <div id="dvIowaWeather"></div>
@@ -100,7 +109,7 @@
                     <canvas id="cvsMainCanvas" width="500" height="500"></canvas>
                     <br />
                     <input type="button" id="btnUp" value="  +  " onclick="clickUp()"/>
-                    <input type="button" id="btnDown" value="  -  " onclick="clickDown()"/>
+                    <input type="button" id="btnDown" value="  -  " onclick="clickDown()"/>                                    
                 </div>  
                 <div id="dvUnixClock">
                     <p id="clockNumber">000</p>
@@ -256,7 +265,7 @@
                     <input type='button' id='btnAddPost' value='Add a Post' />					
                     <?php
                     //Declare variables
-                    $db = mysqli_connect('localhost', '*', '*', 'bulletinBoard');
+                    $db = mysqli_connect('localhost', 'web', 'webaccess', 'bulletinBoard');
                     $query = "CALL bulletinBoard.sp_select_all_posts();";
 
                     //Test for any connection errors
@@ -269,14 +278,20 @@
 
                         //Test to see if there are any results
                         if ($result->num_rows > 0) {
-                            
+
                             //Loop through the results and append to the page
                             $outputString = "";
                             while ($row = $result->fetch_assoc()) {
                                 $name = $row['UserName'];
                                 $content = $row['Content'];
                                 $date = $row['PostDate'];
-                                $outputString = printResult($name, $content, $date).$outputString;
+                                $owner = $row['IsOwner'];
+
+                                $name = str_replace("<", "&#60", $name);
+                                $content = str_replace("<", "&#60", $content);
+
+                                $outputString = printResult($name, $content,
+                                                $date, $owner) . $outputString;
                             }
                             echo $outputString;
                         } else {
@@ -287,17 +302,22 @@
                         $db->close();
                     }
 
-                    function printResult($name, $content, $date) {
+                    function printResult($name, $content, $date, $owner) {
 
                         $output = "";
-                        
+
                         $output .= "<div class='bulletinBoardPost'
                             style='border: 1px solid #c5c5c5; 
                             margin: 5px 0px 5px 0px; 
                             padding: .5em;
                             border-radius: 10px;
                             background-color: #f6f6f6'>";
-                        $output .= "<h3>" . $name . " On " . $date . "</h3>";
+                        if ($owner) {
+                            $output .= "<h3 style='color: blue;'>" . $name .
+                                    " On " . $date . "</h3>";
+                        } else {
+                            $output .= "<h3>" . $name . " On " . $date . "</h3>";
+                        }
                         $output .= "<p>" . $content . "</p>";
                         $output .= "</div>";
                         return $output;
@@ -317,9 +337,130 @@
                             <input type="submit" value="Submit" id="btnSubmitPost"/>
                         </form>
                         <p>WARNING: Posts are for public viewing and are 
-                        liable to be removed at administrator discretion</p>
+                            liable to be removed at administrator discretion</p>
                     </div>
-                </div>
+                </div>                
+                <div id="dvCalculator">
+                    <input type="text" value="0" id="calcOutput" disabled/>
+                    <table width="300">
+                        <tr>
+                            <td>
+                                <input type="button" id="calcBtnPi" value="π" class="calcConst"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnSqrRoot" value="√" class="calcEval"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnSin" value="sin" class="calcEval"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnClr" value="C" class="calcOther"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnClra" value="CE" class="calcOther"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnBK" value="<-" class="calcOther"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnDiv" value="/" class="calcOperator"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="button" id="calcBtnE" value="e" class="calcConst"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnSqr" value="x²" class="calcEval"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnCos" value="cos" class="calcEval"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtn7" value="7" class="calcNumber"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtn8" value="8" class="calcNumber"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtn9" value="9" class="calcNumber"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnMult" value="x" class="calcOperator"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="button" id="calcBtnFact" value="!" class="calcEval"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnCub" value="x³" class="calcEval"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnTan" value="tan" class="calcEval"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtn4" value="4" class="calcNumber"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtn5" value="5" class="calcNumber"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtn6" value="6" class="calcNumber"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnSub" value="-" class="calcOperator"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="button" id="calcBtnAbs" value="abs" class="calcEval"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnPwr" value="^" class="calcOperator"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnMod" value="%" class="calcOperator"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtn1" value="1" class="calcNumber"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtn2" value="2" class="calcNumber"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtn3" value="3" class="calcNumber"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnAdd" value="+" class="calcOperator"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="button" id="calcBtnRound" value="round" class="calcEval"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnNtLog" value="ln" class="calcEval"/>
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnLog" value="log" class="calcEval"/>
+                            </td>
+                            <td>
+                                <!-- input type="button" id="calcBtnNegative" value="±" class="calcNumMod"/> -->
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtn0" value="0" class="calcNumber"/>
+                            </td>
+                            
+                            <td>
+                                <!-- <input type="button" id="calcBtnDecimal" value="." class="calcNumMod"/> -->
+                            </td>
+                            <td>
+                                <input type="button" id="calcBtnEval" value="=" class="calcOther"/>
+                            </td>
+                        </tr>
+                    </table>
+                </div>               
             </div>            
         </main>
     </body>
