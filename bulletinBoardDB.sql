dd24b177882d00fc54823c2a47a2967b1fe682bb
tlickteig <57294226+tlickteig@users.noreply.github.com>
2020-02-25 13:22:11 -0600

    Update bulletinBoardDB.sql

--- bulletinBoardDB.sql
+++ bulletinBoardDB.sql
@@ -6,7 +6,7 @@ USE bulletinBoard;
 
 DROP USER IF EXISTS 'web'@'%';
 
-CREATE USER 'web'@'%' IDENTIFIED WITH mysql_native_password BY 'webaccess';
+CREATE USER 'web'@'%' IDENTIFIED WITH mysql_native_password BY '****';
 
 -- Create table for storing posts
 CREATE TABLE postRecord (
@@ -122,26 +122,3 @@ CALL sp_insert_post("Timothy Lickteig", "This is the third post");
 CALL sp_insert_post("Timothy Lickteig", "This is the fourth post");
 CALL sp_insert_post("Timothy Lickteig", "This is the fifth post");
 */
-
-INSERT INTO postRecord
-(PostDate, UserName, Content, IsOwner)
-VALUES
-("2020-02-15", "The Minivan Master", "This is a post", 1),
-("2020-02-15", "Tony Farrington", "NSFW", 0),
-("2020-02-15", "UwU", "OwO", 0),
-("2020-02-15", "A Guy", "BMW's > Toyota's", 0),
-("2020-02-16", "Cody Freeberg", "Cody was here.", 0),
-("2020-02-18", "Tim Lickteig", "Tony sucks", 0),
-("2020-02-18", "Bob Trapp", "More essay's", 0),
-("2020-02-18", "Thicc dip sticc", "suh", 0),
-("2020-02-18", "Toothpaste man", "Motives are still unknown", 0),
-("2020-02-18", "Jerry Glasgow", "42.146775, -91.744898", 0),
-("2020-02-18", "L33t hacker", '&#60script>console.log("Hello World!");&#60/script>', 0),
-("2020-02-19", "Gooder", "Hi Tim!", 0),
-("2020-02-19", "Tony Farrington", "@Tim Lickteig (Not the real one, the one below), clearly has no sense of humor!", 0),
-("2020-02-19", "Anonymous Yanker", "Hey Tim, Put some more fun stuff on this page would you. I really get bored while cruising the net and I just feel like if we had some more great, compelling content like the items found on this site, well the world would be a better place. Please make more... I'll be watching p.s. I would like to see a picture of this homemade truck.", 0),
-("2020-02-20", "Schwarzbraun is die hasulnuts", "YA YA YA DA DA", 0),
-("2020-02-20", "Test for html break", '&#60h1>Test&#60/h1>', 0),
-("2020-02-20", "Last test", '&#60/textarea>&#60input type="submit" value="Submit" id="btnSubmitPost" class="ui-button ui-corner-all ui-widget" role="button">&#60/form>&#60p>Did this break?&#60/p>', 0),
-("2020-02-21", "Gooder", "Has big pp", 0)
-;
